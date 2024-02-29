fcc4763a6076ae668cdb646c8c484cdb42a350e3
Matej7418 <58819158+Matej7418@users.noreply.github.com>
2024-02-29 12:06:30 +0100

    fe

--- app/src/components/__tests__/HelloWorld.spec.ts
+++ app/src/components/__tests__/HelloWorld.spec.ts
@@ -5,7 +5,8 @@ import HelloWorld from '../HelloWorld.vue'
 
 describe('HelloWorld', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
+    const msg = 'Hello Vitest';
+    const wrapper = mount(HelloWorld, { props: { msg: msg } });
+    expect(wrapper.text()).toContain(msg);
   })
 })
