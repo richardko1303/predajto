9733e3b43ba614c120104f658ddc21b80f57c3c0
Matej7418 <58819158+Matej7418@users.noreply.github.com>
2024-04-04 09:08:44 +0200

    tailwind in global.sass

--- app/src/plugins/app@landing/landing.vue
+++ app/src/plugins/app@landing/landing.vue
@@ -1,11 +1,14 @@
 <template>
-	<div>
-		Something
+	<div class="h-screen w-screen bg-primary-light-green">
+		<section class="search">
+            <h1 class="text-primary-white">AAA</h1>
+        </section>
 	</div>
 </template>
 
 <script>
 export default {
-
+    name: "Landing"
 }
+
 </script>
