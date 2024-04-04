6d0c53bb223c157667d75e476c123a64e0888563
Matus Luptak <113360574+Shuterix@users.noreply.github.com>
2024-04-04 10:00:46 +0200

    [Navbar] fixed height

--- app/src/plugins/app@footer/footer.vue
+++ app/src/plugins/app@footer/footer.vue
@@ -1,24 +1,16 @@
 <template>
 	<footer>
-	  	<div>
-			<div>
-		  		<p>Vytvorené pomocou <span>Predajto Studio™</span></p>
-			</div>
-	  	</div>
+		<p>Vytvorené pomocou <span>Predajto Studio™</span></p>
 	</footer>
 </template>
 
 <style scoped lang="sass">
 
 	footer 
-		@apply bg-primary-dark-green w-full
-		div
-			@apply mx-auto max-w-7xl px-3 py-6 md:flex md:items-center md:justify-between lg:px-8
-			div
-				@apply md:order-1 mt-0
-				p
-					@apply text-center text-lg leading-5 text-white font-light
-					span
-						@apply font-semibold
+		@apply bg-primary-dark-green w-full h-[90px] flex justify-center items-center
+		p
+			@apply text-center text-lg leading-5 text-white font-light
+			span
+				@apply font-semibold
 
 </style>
