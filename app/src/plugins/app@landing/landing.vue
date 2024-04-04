b4864cb4d4f5bc9b62ea10eb95c4a31d881c2d01
Matej7418 <58819158+Matej7418@users.noreply.github.com>
2024-04-04 09:58:35 +0200

    landing page init

--- app/src/plugins/app@landing/landing.vue
+++ app/src/plugins/app@landing/landing.vue
@@ -1,14 +1,34 @@
 <template>
-	<div class="h-screen w-screen bg-primary-light-green">
-		<section class="search">
-            <h1 class="text-primary-white">AAA</h1>
+	<div class="h-screen w-screen bg-primary-white">
+		<section class="
+		    sec-search flex justify-center py-16
+		    bg-gradient-to-r from-primary-light-green to-primary-white
+        ">
+            <search/>
+        </section>
+
+        <section class="options flex justify-center py-8">
+            <p class="text-primary-gray text-[16px]">Mesto:</p>
+        </section>
+
+        <section class="main flex justify-around">
+            <div>
+                <Categories/>
+            </div>
+            <div>
+                <p>A</p>
+            </div>
         </section>
 	</div>
 </template>
 
 <script>
+import Search from "./_components/search.vue";
+import Categories from "./_components/categories.vue";
+
 export default {
-    name: "Landing"
+    name: "Landing",
+    components: {Categories, Search }
 }
 
 </script>
