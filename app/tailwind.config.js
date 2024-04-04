7c5499f2813605004afc8d376253e6ea0b6eb74a
Matus Luptak <113360574+Shuterix@users.noreply.github.com>
2024-04-04 08:48:57 +0200

    [Tailwind config] added primary colors

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+	colors: {
+		'primary-light-green': '#44D7C6',
+		'primary-black': '#011312',
+		'primary-white': '#E1FFFD',
+		'primary-gray': '64748B',
+		'primary-dark-green': '007164'
+	},
     extend: {},
   },
   plugins: [],
