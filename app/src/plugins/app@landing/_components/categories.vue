841c9e7e457ee54be01d6cdf4d1e99136e6c84c7
Matej7418 <58819158+Matej7418@users.noreply.github.com>
2024-04-10 22:16:34 +0200

    [Categories] add subcategories

--- app/src/plugins/app@landing/_components/categories.vue
+++ app/src/plugins/app@landing/_components/categories.vue
@@ -3,10 +3,19 @@
         <p class="text-primary-gray font-semibold pt-2">KATEGÓRIE</p>
         <div class="p-2 gap-1">
             <ul class="pl-4 text-lg">
-                <li v-for="c in categories" class="flex flex-row align-middle">
-                    <p>{{ c.name }}</p>
-                    <div class="ml-2 px-2 bg-primary-light-green hover:bg-primary-dark-green rounded-lg">
-                        <p class="text-white">+</p>
+                <li v-for="c in categories" class="flex flex-col">
+                    <div class="flex flex-row align-middle">
+                        <p class="w-32">{{ c.name }}</p>
+                        <div
+                            v-if="(c?.subcategories?.length ?? 0) > 0"
+                            @click="c.expanded = !c.expanded"
+                            class="ml-2 px-2 w-7 text-center bg-primary-light-green hover:bg-primary-dark-green rounded-lg"
+                        >
+                            <p class="text-white">{{ (c.expanded ? '-' : '+') }}</p>
+                        </div>
+                    </div>
+                    <div v-if="c.expanded" v-for="subc in c.subcategories" class="pl-2">
+                        <p class="text-sm text-primary-gray hover:text-primary-black">{{ subc }}</p>
                     </div>
                 </li>
             </ul>
@@ -15,24 +24,38 @@
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue'
+import { defineComponent } from 'vue'
+
+interface Category {
+    name: string
+    expanded?: boolean | null
+    subcategories?: string[]
+}
+
+let categories: Category[] = [
+    { name: "Vsetky Produkty" },
+    {
+        name: "Elektronika",
+        subcategories: [
+            "Telefony",
+            "Prislusenstvo",
+            "Kable"
+        ]
+    },
+    {
+        name: "Sport",
+        subcategories: ["nieco", "nieco 2"]
+    },
+    { name: "Hobby" },
+    { name: "Upratovanie" },
+    { name: "Ostatne" },
+]
 
 export default defineComponent({
     name: "Categories",
     data() {
         return {
-            categories: [
-                {
-                    name: "Vsetky Produkty",
-                    expanded: false,
-                    subcategories: []
-                },
-                {
-                    name: "Elektronika",
-                    expanded: false,
-                    subcategories: []
-                },
-            ]
+            categories: categories
         }
     }
 })
