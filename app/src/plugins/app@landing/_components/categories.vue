0a8945aca0ccaa8deda6844cde7f06a1c1185944
Matej7418 <58819158+Matej7418@users.noreply.github.com>
2024-04-11 09:21:33 +0200

    [Categories] add icon

--- app/src/plugins/app@landing/_components/categories.vue
+++ app/src/plugins/app@landing/_components/categories.vue
@@ -1,21 +1,22 @@
 <template>
     <div class="">
-        <p class="text-primary-gray font-semibold pt-2">KATEGÓRIE</p>
-        <div class="p-2 gap-1">
-            <ul class="pl-4 text-lg">
-                <li v-for="c in categories" class="flex flex-col">
-                    <div class="flex flex-row align-middle">
-                        <p class="w-32">{{ c.name }}</p>
+        <p class="text-primary-gray font-semibold py-2">KATEGÓRIE</p>
+        <div class="py-2 gap-1">
+            <ul class="text-lg">
+                <li v-for="c in categories" class="flex flex-col mb-1">
+                    <div class="flex flex-row">
+                        <img class="w-7 select-none" src="@/assets/category-table.svg" alt="Category Icon" />
+                        <p class="ml-2 w-44 overflow-scroll">{{ c.name }}</p>
                         <div
                             v-if="(c?.subcategories?.length ?? 0) > 0"
                             @click="c.expanded = !c.expanded"
-                            class="ml-2 px-2 w-7 text-center bg-primary-light-green hover:bg-primary-dark-green rounded-lg"
+                            class="px-2 w-7 text-center cursor-pointer bg-primary-light-green bg-opacity-30 hover:bg-opacity-100 rounded-lg"
                         >
-                            <p class="text-white">{{ (c.expanded ? '-' : '+') }}</p>
+                            <p class="text-primary-black select-none">{{ (c.expanded ? '-' : '+') }}</p>
                         </div>
                     </div>
-                    <div v-if="c.expanded" v-for="subc in c.subcategories" class="pl-2">
-                        <p class="text-sm text-primary-gray hover:text-primary-black">{{ subc }}</p>
+                    <div v-if="c.expanded" v-for="subc in c.subcategories" class="pl-9">
+                        <p class="text-sm cursor-pointer select-none text-primary-gray hover:text-primary-black">{{ subc }}</p>
                     </div>
                 </li>
             </ul>
@@ -55,7 +56,7 @@ export default defineComponent({
     name: "Categories",
     data() {
         return {
-            categories: categories
+            categories: categories,
         }
     }
 })
